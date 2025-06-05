3feb86f866b428ee1b78968e222cee6570a38658
Yuxuan Sun <hjsdbb1@gmail.com>
2025-06-06 04:36:10 +0800

    core: fix en_US language enforcement in cmds (#20)

--- src/SystemInfo.cpp
+++ src/SystemInfo.cpp
@@ -25,7 +25,7 @@
 using namespace Hyprutils::String;
 
 CSystemInternals::CSystemInternals(QObject* parent) : QObject(parent) {
-    setenv("LANG", "en_US.UTF-8", true);
+    setenv("LC_ALL", "en_US.UTF-8", true);
 
     // gather data from os-release
     if (auto data = readFile("/etc/os-release")) {
